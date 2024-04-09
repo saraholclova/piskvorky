3a79cd625fe1a7fc0350ac699f1267f712084f68
Sára Hölclová <sara.holclova@gmail.com>
2024-04-09 08:45:50 +0200

    Přidány posluchaře na všech 10 tlačítek

--- hra.js
+++ hra.js
@@ -1,10 +1,55 @@
 let currentPlayer = 'circle';
 
-const buttonElement = document.querySelector('button:nth-child(1)');
-
 const selectButton = (event) => {
   event.target.classList.add('game__square--circle');
 };
 
-buttonElement.addEventListener('click', selectButton);
+document
+  .querySelector('button:nth-child(1)')
+  .addEventListener('click', selectButton);
+s;
+
+document
+  .querySelector('button:nth-child(2)')
+  .addEventListener('click', selectButton);
+s;
+
+document
+  .querySelector('button:nth-child(3)')
+  .addEventListener('click', selectButton);
+s;
+
+document
+  .querySelector('button:nth-child(4)')
+  .addEventListener('click', selectButton);
+s;
+
+document
+  .querySelector('button:nth-child(5)')
+  .addEventListener('click', selectButton);
+s;
+
+document
+  .querySelector('button:nth-child(6)')
+  .addEventListener('click', selectButton);
+s;
+
+document
+  .querySelector('button:nth-child(7)')
+  .addEventListener('click', selectButton);
+s;
+
+document
+  .querySelector('button:nth-child(8)')
+  .addEventListener('click', selectButton);
+s;
+
+document
+  .querySelector('button:nth-child(9)')
+  .addEventListener('click', selectButton);
+s;
+
+document
+  .querySelector('button:nth-child(10)')
+  .addEventListener('click', selectButton);
 s;
